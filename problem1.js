41382d1a893be55f38140b234efbbcaa337b575e
Sudhir Nandane <105806651+codersudhir@users.noreply.github.com>
2024-12-11 12:35:29 +0000

    solve problem

---
+++ problem1.js
@@ -0,0 +1,31 @@
+const name = "Amolya Sharma";
+
+const lowercasename = name.toLowerCase();
+console.log(lowercasename);
+let count = 1;
+
+let bag = "";
+const arr = [];
+let store = 1;
+for (let i = 0; i < lowercasename.length; i++) {
+    if (arr.includes.name[i]) {
+        arr.push(lowercasename[i] + "-" + count);
+    }
+}
+
+console.log(arr);
+
+// example  2
+const name2 = "Chinmay Kulkarni";
+
+let chars2 = "abcdefghijklnmopqrstuvwxyz";
+let count2 = 1;
+
+const arr2 = [];
+for (let i = 0; i < name2.length; i++) {
+    if (arr2.includes.name2[i]) {
+        arr2.push(name2[i] + "-" + count2);
+    }
+}
+
+console.log(arr2); 
