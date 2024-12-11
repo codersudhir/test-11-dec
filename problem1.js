d90057b79503aa489e9aa2b6aadc66a9aead5ce5
Sudhir Nandane <105806651+codersudhir@users.noreply.github.com>
2024-12-11 12:39:38 +0000

    solve problem

--- problem1.js
+++ problem1.js
@@ -1,31 +1,41 @@
+// Example 1
 const name = "Amolya Sharma";
 
 const lowercasename = name.toLowerCase();
 console.log(lowercasename);
-let count = 1;
 
-let bag = "";
 const arr = [];
-let store = 1;
 for (let i = 0; i < lowercasename.length; i++) {
-    if (arr.includes.name[i]) {
+    if (!arr.some((item) => item.startsWith(lowercasename[i] + "-"))) {
+        let count = 0;
+        for (let j = 0; j < lowercasename.length; j++) {
+            if (lowercasename[i] === lowercasename[j]) {
+                count++;
+            }
+        }
         arr.push(lowercasename[i] + "-" + count);
     }
 }
 
 console.log(arr);
 
-// example  2
+// Example 2
 const name2 = "Chinmay Kulkarni";
-
-let chars2 = "abcdefghijklnmopqrstuvwxyz";
-let count2 = 1;
 
 const arr2 = [];
 for (let i = 0; i < name2.length; i++) {
-    if (arr2.includes.name2[i]) {
+    if (
+        !arr2.some((item) => item.startsWith(name2[i] + "-")) &&
+        name2[i] !== " "
+    ) {
+        let count2 = 0;
+        for (let j = 0; j < name2.length; j++) {
+            if (name2[i] === name2[j]) {
+                count2++;
+            }
+        }
         arr2.push(name2[i] + "-" + count2);
     }
 }
 
-console.log(arr2); 
+console.log(arr2);
